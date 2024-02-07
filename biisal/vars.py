631c16508a05eede6b757f4f2ca1532583080604
Amanvishwakarmabot <147085734+Amanvishwakarmabot@users.noreply.github.com>
2024-02-07 19:31:42 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -12,18 +12,18 @@ bisal_grp = "https://t.me/+PA8OPL2Zglk3MDM1"
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', ''))
-    API_HASH = str(getenv('API_HASH', ''))
-    BOT_TOKEN = str(getenv('BOT_TOKEN' , ''))
+    API_ID = int(getenv('API_ID', '24004349'))
+    API_HASH = str(getenv('API_HASH', '5aabfb11c262b17d568d828a3100f296'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN' , '6568488552:AAGUd-ZIsr9EeflSgbdhIoPXSMnidRdUoPA'))
     name = str(getenv('name', 'bisal_file2link_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', ''))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002062708890'))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002062708890'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "5397984467").split()]
+    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "5977931010").split()]
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'biisal'))
